a748d60a034bd1428b8f35daa0832e9af478c167
Matheus Brito Faria <matheus.brito000@gmail.com>
2021-03-15 18:01:20 -0300

    add more tests

--- r.sh
+++ r.sh
@@ -6,4 +6,10 @@ case "$1" in
     p5) cd test_env/peer_5; python3 peer.py 5005 key-values-files_peer5 10.0.0.2:5002 10.0.0.3:5003 10.0.0.4:5004;;
     c1) cd test_env/client_1; python3 cliente.py 10.0.0.1:5001 1,3,4,5,9;;
     c2) cd test_env/client_2; python3 cliente.py 10.0.0.4:5004 5,6,7;;
+    ct) cd test_env/client_1; python3 cliente.py 10.0.0.1:5001 9;;
+    p11) cd test_env/peer_1; python3 peer.py 5001 key-values-files_peer1 10.0.0.2:5002;;
+    p22) cd test_env/peer_2; python3 peer.py 5002 key-values-files_peer2 10.0.0.3:5003;;
+    p33) cd test_env/peer_3; python3 peer.py 5003 key-values-files_peer3 10.0.0.5:5005;;
+    p44) cd test_env/peer_4; python3 peer.py 5004 key-values-files_peer4 10.0.0.5:5005;;
+    p55) cd test_env/peer_5; python3 peer.py 5005 key-values-files_peer5 10.0.0.4:5004;;
 esac
